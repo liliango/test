fefcd8cf9523f747da10ba5c7a10d3811850933a
liliango <liliangdk@163.com>
2021-07-14 03:13:56 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# 基于React官方脚手架-create-react-app
+
+> 这个脚手架使用create-react-app 创建的脚手架
+
+## Features 功能特性
+
+- antd 按需加载样式webpack配置
+- react-hot-loader 热加载
+- 引入fontawesome
+- 使用react-group-transition替代swiper做轮播图动画
+
+## 1. start
+
+```
+$ git clone https://github.com/nqmysb/shared-library-ui.git
+$ cd shared-library-ui
+$ npm install 或者 yarn install
+
+```
+> npm如果速度比较慢，请更换镜像为淘宝镜像
+
+## 2. dev
+### 2.1 开发调试
+```
+//打包完成，启动项目
+$ npm start 或者 yarn start
+```
+
+查看效果` http://127.0.0.1:3000`
+![](https://s2.ax1x.com/2019/03/14/AAbjyt.png)
+![](https://s2.ax1x.com/2019/03/14/AAbXQI.png)
+
+### 2.2 代码检查
+```
+$ npm run lint
+```
+
+### 2.3 测试用例
+```
+$ npm run test:watch
+```
+
+### 2.4 产出资源
+```
+$ npm run build
+```
+
+## 3. 技术栈
+
+- [x] [Webpack](https://webpack.github.io)
+- [x] [React](https://facebook.github.io/react/)
+- [x] [ES6](http://es6.ruanyifeng.com/)
+- [x] [Redux](https://github.com/rackt/redux)
+- [x] [React-router](https://github.com/rackt/react-router-redux)
+- [x] [react-router-redux](https://github.com/reactjs/react-router-redux)
+
+
+美工原型参考：https://www.wenkuzhijia.cn/
+
+
+
---
+++ src/configureStore.js
@@ -0,0 +1,44 @@
+import { createBrowserHistory,createHashHistory } from 'history'
+import { applyMiddleware, compose, createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+// createLogger 不是default 需要{}  默认导出 logger
+// import { createLogger } from 'redux-logger'
+// const logger = createLogger({
+//   // ...options
+// });
+// 或使用默认导入
+
+// import logger from 'redux-logger'
+import { createLogger }  from 'redux-logger';
+import { routerMiddleware } from 'connected-react-router'
+import createRootReducer from './reducers/index'
+
+
+// export const history = createBrowserHistory()
+
+export const history = createHashHistory()
+
+const logger = createLogger({ collapsed: true });
+
+const middleware = routerMiddleware(history) // for dispatching history actions
+
+const middlewares = [thunk,middleware];
+
+// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+if(process.env.NODE_ENV === 'development') {
+    middlewares.push(logger)    
+}
+
+const store = createStore(
+  createRootReducer(history), // root reducer with router state
+  composeWithDevTools(applyMiddleware(...middlewares))
+)
+
+
+export default store
+
+
+
+
+
